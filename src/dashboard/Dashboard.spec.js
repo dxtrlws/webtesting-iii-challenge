d4561c7666e523a10ccd0b64470e76488d7556da
Dexter Lewis <dexter.lewis@gmail.com>
2019-06-26 19:04:10 -0400

    complete testing of dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,25 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { render } from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
+
+import Dashboard from './Dashboard';
 // Test away
+
+describe('<Dashboard />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard />)
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+    it('should render dashboard', () => {
+        render(<Dashboard />)
+    })
+    it('should display controls component', () => {
+        const { getByText } = render(<Dashboard/>)
+        getByText(/lock gate/i)
+    })
+    it('should display Display component', () => {
+        const { getByText } = render(<Dashboard />)
+        getByText(/unlocked/i)
+    })
+}) 
